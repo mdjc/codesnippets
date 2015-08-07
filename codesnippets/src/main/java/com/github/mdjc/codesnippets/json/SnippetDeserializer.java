c54346354a90f33a512df767354381b3e1d51dd6
mdjc <mirnadjc@gmail.com>
2015-08-07 14:33:48 -0400

    refactor to manage dezeralization exceptions in base class SimpleDeserializer

--- codesnippets/src/main/java/com/github/mdjc/codesnippets/json/SnippetDeserializer.java
+++ codesnippets/src/main/java/com/github/mdjc/codesnippets/json/SnippetDeserializer.java
@@ -7,18 +7,12 @@ public class SnippetDeserializer extends SimpleDeserializer<Snippet> {
 
 	@Override
 	protected Snippet deserialize(JsonNode tree) throws Exception {
-		try {
-			long id = tree.get("id").asLong();
-			String title = tree.get("title").asText();
-			String code = tree.get("code").asText();
-			String language = tree.get("language").asText();
-			String description = tree.get("description").asText();
-
-			return Snippet.of(id, title, code, language, description);
-		} catch (Exception e) {
-			throw new IllegalArgumentException(
-					"Snippet was no received properly. Json content required : {id:0, title : <title>, code: <code>, language: <language>, description: <description>}");
-		}
+		long id = tree.get("id").asLong();
+		String title = tree.get("title").asText();
+		String code = tree.get("code").asText();
+		String language = tree.get("language").asText();
+		String description = tree.get("description").asText();
+		return Snippet.of(id, title, code, language, description);
 	}
 
 }
