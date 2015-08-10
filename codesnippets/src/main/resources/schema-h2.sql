94703436ec72af30b2d7d85afb7aec4a96cb1e6d
mdjc <mirnadjc@gmail.com>
2015-08-10 19:12:12 -0400

    rename for h2 use only

---
+++ codesnippets/src/main/resources/schema-h2.sql
@@ -0,0 +1,16 @@
+CREATE TABLE USERS (
+	user_id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	user_name VARCHAR(35) NOT NULL UNIQUE,
+	user_email VARCHAR(60) NOT NULL,
+	user_password VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE snippets (
+	snippet_id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	snippet_user BIGINT UNSIGNED NOT NULL REFERENCES users(user_id),
+	snippet_title VARCHAR(60) NOT NULL,
+	snippet_code VARCHAR(4000) NOT NULL,
+	snippet_description VARCHAR(330),
+	snippet_language VARCHAR(40),
+	UNIQUE(snippet_user, snippet_title)
+);
