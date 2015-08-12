48c84f7b307375baf2d4943407cb13111d6a9bf1
mdjc <mirnadjc@gmail.com>
2015-08-12 14:58:29 -0400

    add btnAutoCollapse directive

---
+++ codesnippets/src/main/resources/static/assets/directives/btn-auto-collapse.directive.js
@@ -0,0 +1,31 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('codesnippetsApp')
+		.directive('btnAutoCollapse', directive);
+	
+	function directive() {
+		var dir = {
+			restrict: 'A',
+		    scope: {},
+			link: link
+		};
+		return dir;
+
+		function link(scope, element, attrs) {	
+			element.on('click', function(event) {			   
+				$(".navbar-collapse.in").collapse('hide');
+			 });
+		}
+	}
+})();
+
+
+
+
+
+
+
+
+
--- codesnippets/src/main/resources/static/assets/layout/topnav.controller.js
+++ codesnippets/src/main/resources/static/assets/layout/topnav.controller.js
@@ -12,18 +12,6 @@
 		
 		vm.signOut = signOut;
 		
-		activate();
-		
-		function activate() {
-			//automatically collapse navbar when link clicked AND navbar is compacted (because of small screen)
-		    //see: https://github.com/twbs/bootstrap/issues/12852
-		    $(document).on('click', '.navbar-collapse.in', function(e) {
-		        if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) {
-		            $(this).collapse('hide');
-		        }
-		    });
-		}
-		
 		function signOut() {
 			authentication.signOut().success(successCallback); 
 		}
